b9865999c1d61b927fc4a102d349928eed0f7fd9
Sangeetha Shanmugam <iamsangshan@gmail.com>
2020-08-29 02:47:49 +0530

    comments

--- random/easy/reverse_a_signed_integer.c
+++ random/easy/reverse_a_signed_integer.c
@@ -17,6 +17,11 @@ Output: 21
 Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
+
+
+
+****** This code assumes that, we are dealing with 31 bits. and that sign will be taken care 
+
  */
 
 int reverse(int x){
